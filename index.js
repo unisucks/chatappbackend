4e1644aa50ad6db54a1539220469a111fae324a0
tharchit <tharchit262000@gmail.com>
2024-12-13 21:08:51 +0630

    backend

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ const corsOptions = {
 };
 const PORT = process.env.PORT || 3005;
 mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`);
-
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://chatappfrontend-iota.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Preflight request
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
